637f12034d1e30bab016541942698984462ce560
Mandy Noto <mandynoto@outlook.com>
2025-08-22 21:14:50 -0700

    feat: implement and use Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/components/NavBar";
+
 export default function Home() {
   return (
     <main className="mx-auto flex h-screen max-w-2xl flex-col">
-      Navbar
+      <Navbar />
       <div className="flex-1 overflow-y-auto">MessageWindow</div>
       <footer className="p-4">ChatInput</footer>
     </main>
---
+++ components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="bg-transparent p-4">
+      <div className="container mx-auto">
+        <div className="flex items-center">
+          <div className="text-white font-bold text-xl">Logo</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
