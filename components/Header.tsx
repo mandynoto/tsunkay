a6cdc4393319712ea71e705e5d1ed0ac3b35768d
Mandy Noto <mandynoto@outlook.com>
2025-08-26 01:05:34 -0700

    feat: add GitHub icon before the source

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,21 @@
 import CTAButton from "./CTAButton";
+import { Github } from "lucide-react";
 import React from "react";
 
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 w-full z-50 bg-[#fdfdfd] shadow-md px-6 py-4 flex justify-between items-center">
-      <div className="text-black text-xl font-bold">Tsunkay</div>
-      <CTAButton>Sign Up</CTAButton>
+      <div className="text-slate-600 text-s text-xl">Tsunkay</div>
+      <CTAButton
+        isFilled={false}
+        href="https://github.com/mandynoto/tsunkay"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center gap-2"
+      >
+        <Github size={16} />
+        Source
+      </CTAButton>
     </header>
   );
 };
