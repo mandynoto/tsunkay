17bfdeb562f3136c4cccaf6951e8b8a935f0f02b
Mandy Noto <mandynoto@outlook.com>
2025-08-30 19:59:01 -0700

    feat: replace source code button icon as a footer icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,19 @@
+import { VscGithubAlt } from "react-icons/vsc";
+
 export default function Footer() {
   return (
-    <footer className="py-6">
+    <footer className="py-6 flex justify-center items-center relative">
       <p className="text-center text-xs text-gray-400">
         Tsunkay can make mistakes, so let&apos;s double-check responses
       </p>
+      <a
+        href="https://github.com/mandynoto/tsunkay"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center text-gray-400 absolute right-6"
+      >
+        <VscGithubAlt size={16} />
+      </a>
     </footer>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,9 @@
-import CTAButton from "./CTAButton";
-import { Github } from "lucide-react";
 import React from "react";
 
 const Header = () => {
   return (
     <header className="w-full z-50 bg-[#fdfdfd] border-b border-slate-200 px-6 py-4 flex justify-between items-center">
       <div className="text-slate-600 text-s text-xl">Tsunkay</div>
-      <CTAButton
-        isFilled={false}
-        href="https://github.com/mandynoto/tsunkay"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex items-center gap-2"
-      >
-        <Github size={16} />
-        Source
-      </CTAButton>
     </header>
   );
 };
